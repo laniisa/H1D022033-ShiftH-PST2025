f5acfe9e6bb2e1d5be654f16735980fb973ce456
laniisa <laniisa2207@gmail.com>
2025-04-29 12:19:41 +0700

    LCD

--- LCD/src/main.cpp
+++ LCD/src/main.cpp
@@ -2,15 +2,15 @@
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 26, 2);
-int SDA_pin = D4;
-int SCL_pin = D2;
+int SDA_pin = D3;
+int SCL_pin = D4;
 
 void setup() {
   Wire.begin(SDA_pin,SCL_pin);
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print("Praktikum Sistem Tertanam");
+  lcd.print("Solani H1D022033");
   lcd.setCursor(0,1);
   lcd.print("Informatika");
 }
